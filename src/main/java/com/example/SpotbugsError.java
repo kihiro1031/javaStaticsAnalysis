58a902b1cba3a542d0d7b9f51b78145450c2260a
akihiro-okiguchi <akihiro-okiguchi@bm-sms.co.jp>
2025-06-10 18:12:25 +0900

    問題のないコードを追加した時、エラー検知されないことを確認する

--- src/main/java/com/example/SpotbugsError.java
+++ src/main/java/com/example/SpotbugsError.java
@@ -2,7 +2,9 @@ package com.example;
 
 public class SpotbugsError {
     public static void main(String[] args) {
+        System.out.println("問題ないコード");
+
         String str = null;
-        System.out.println(str.length()); 
+        System.out.println(str.length());
     }
 }
